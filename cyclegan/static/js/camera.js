e8551ede23c6d9c57c999e34c2eb35220a5e7165
Ankur Gupta <rukna1000@gmail.com>
2020-05-24 01:49:32 -0400

    Activate camera via a button

--- cyclegan/static/js/camera.js
+++ cyclegan/static/js/camera.js
@@ -1,18 +1,28 @@
-var width = 320;
-var height = undefined;
+// Hyper parameters
+const width = 320;
 
+// Instance variables
 var streaming = false;
 
-var video = null;
-var canvas = null;
-var photo = null;
-var startButton = null;
+var video = undefined;
+var canvas = undefined;
+var photo = undefined;
+var cameraButton = undefined;
+var height = undefined;
 
-function startup() {
+
+// Functions
+function init() {
   video = document.getElementById('video');
   canvas = document.getElementById('canvas');
   photo = document.getElementById('photo');
-  startButton = document.getElementById('startButton');
+  cameraButton = document.getElementById('cameraButton');
+
+  cameraButton.addEventListener('click', activate_webcam, false);
+}
+
+function activate_webcam() {
+  if(streaming) return;
 
   navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(stream => {
@@ -22,18 +32,18 @@ function startup() {
     .catch(err => console.log('Error playing video: ' + err));
 
   video.addEventListener('canplay', function(event) {
-    if(!streaming) {
-      height = video.videoHeight / (video.videoWidth / width);
+    height = video.videoHeight / (video.videoWidth / width);
 
-      if(isNaN(height)) height = width / (4 / 3);
+    if(isNaN(height)) height = width / (4 / 3);
 
-      video.setAttribute('width', width);
-      video.setAttribute('height', height);
-      canvas.setAttribute('width', width);
-      canvas.setAttribute('height', height);
-      streaming = true;;
-    }
+    video.setAttribute('width', width);
+    video.setAttribute('height', height);
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+    streaming = true;;
   }, false);
 }
 
-window.addEventListener('load', startup, false);
+
+// Initialize script on window load
+window.addEventListener('load', init, false);
