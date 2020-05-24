9a10b31236f9177b23e3060b454be1708e1782a2
Ankur Gupta <rukna1000@gmail.com>
2020-05-24 09:37:42 -0400

    Control camera with css instead of JavaScript

--- cyclegan/static/js/camera.js
+++ cyclegan/static/js/camera.js
@@ -1,5 +1,5 @@
 // Hyper parameters
-const width = 320;
+const sideLen = 320;
 
 // Instance variables
 var streaming = false;
@@ -8,7 +8,6 @@ var video = undefined;
 var canvas = undefined;
 var photo = undefined;
 var cameraButton = undefined;
-var height = undefined;
 
 
 // Functions
@@ -32,14 +31,6 @@ function activate_webcam() {
     .catch(err => console.log('Error playing video: ' + err));
 
   video.addEventListener('canplay', function(event) {
-    height = video.videoHeight / (video.videoWidth / width);
-
-    if(isNaN(height)) height = width / (4 / 3);
-
-    video.setAttribute('width', width);
-    video.setAttribute('height', height);
-    canvas.setAttribute('width', width);
-    canvas.setAttribute('height', height);
     streaming = true;;
   }, false);
 }
