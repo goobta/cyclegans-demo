29ebe0a1c6d14f4053d77bd1c95a29debe3c8ca3
Ankur Gupta <rukna1000@gmail.com>
2020-05-25 12:40:02 -0400

    comment utils

--- cyclegan/utils.py
+++ cyclegan/utils.py
@@ -4,13 +4,29 @@ import PIL.Image
 import io
 
 
-def requestb64_to_image(req: str):
+def requestb64_to_image(req: str) -> PIL.Image:
+  """Convert a flask request into a PIL image 
+
+  Args:
+    req (str): flask request body
+
+  Returns:
+    PIL.Image: image decoded from the base64 request
+  """
   prefix, _, b64 = req.partition(',')
   buf = io.BytesIO(base64.b64decode(b64))
   return PIL.Image.open(buf)
 
 
-def numpy_img2b64req(img: np.ndarray):
+def numpy_img2b64req(img: np.ndarray) -> str:
+  """Convert numpy image into base64 encoded image request body
+
+  Args:
+    img (np.ndarray): image array. should be of shape (width, height, channels)
+
+  Returns:
+    str: base64 encoded image body
+  """
   amin = np.amin(img)
   amax = np.amax(img)
   norm = (255 * (img - amin) / (amax - amin)).astype(np.uint8)
