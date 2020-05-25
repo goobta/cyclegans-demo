66bd2db036a811057ae76b1d4e4e4531cd46de24
Ankur Gupta <rukna1000@gmail.com>
2020-05-25 13:11:15 -0400

    add docstring comments

--- cyclegan/utils.py
+++ cyclegan/utils.py
@@ -41,11 +41,18 @@ def numpy_img2b64req(img: np.ndarray) -> str:
 
 
 def assert_downloaded(name: str, url:str, fn: str = 'latest_net_G.pth') -> bool:
+  """Ensure that model is downloaded
+
+  Args:
+    name (str): name of model
+    url (str): url to download the model
+    fn (str, optional): file name to save the model as . Defaults to
+      'latest_net_G.pth'.
+  """
   if os.path.isfile('bin/models/{}/{}'.format(name, fn)):
-    return True
+    return 
   
   os.makedirs('bin/models/{}'.format(name), exist_ok=True)
 
   print('downloading {} model'.format(name))
   urllib.request.urlretrieve(url, 'bin/models/{}/{}'.format(name, fn))
-  return True
