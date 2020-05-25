d3340ea519ee50c69525081dcc2a67afd4e8f974
Ankur Gupta <rukna1000@gmail.com>
2020-05-25 11:53:06 -0400

    fix flask async issue

--- cyclegan/utils.py
+++ cyclegan/utils.py
@@ -13,16 +13,11 @@ def requestb64_to_image(req: str):
 
 
 def numpy_img2b64(img: np.ndarray):
-  plt.imshow(img)
-  plt.show()
-
   amin = np.amin(img)
   amax = np.amax(img)
   norm = (255 * (img - amin) / (amax - amin)).astype(np.uint8)
 
   pil_img = PIL.Image.fromarray(norm)
-  plt.imshow(pil_img)
-  plt.show()
   buffer = io.BytesIO()
   pil_img.save(buffer, format='PNG')
   return base64.b64encode(buffer.getvalue()).decode('utf-8')
