8c23b237a355ff8910c84caf3f25dc5e4ff18a36
Ankur Gupta <rukna1000@gmail.com>
2020-05-25 10:46:52 -0400

    actually works?

--- cyclegan/utils.py
+++ cyclegan/utils.py
@@ -4,6 +4,8 @@ import PIL.Image
 import io
 
 
+from matplotlib import pyplot as plt
+
 def requestb64_to_image(req: str):
   prefix, _, b64 = req.partition(',')
   buf = io.BytesIO(base64.b64decode(b64))
@@ -11,7 +13,16 @@ def requestb64_to_image(req: str):
 
 
 def numpy_img2b64(img: np.ndarray):
-  pil_img = PIL.Image.fromarray((img * 255).astype(np.uint8))
+  plt.imshow(img)
+  plt.show()
+
+  amin = np.amin(img)
+  amax = np.amax(img)
+  norm = (255 * (img - amin) / (amax - amin)).astype(np.uint8)
+
+  pil_img = PIL.Image.fromarray(norm)
+  plt.imshow(pil_img)
+  plt.show()
   buffer = io.BytesIO()
   pil_img.save(buffer, format='PNG')
   return base64.b64encode(buffer.getvalue()).decode('utf-8')
